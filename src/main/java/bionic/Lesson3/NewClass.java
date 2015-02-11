3567d6654772ef2715503c115b5900b5168bcd7a
[Aleks Kutyna] <alekskt@gmail.com>
2015-02-11 19:27:04 +0200

    delete target

---
+++ src/main/java/bionic/Lesson3/NewClass.java
@@ -0,0 +1,10 @@
+package bionic.Lesson3;
+
+/**
+ * Created by c2612 on 11.02.2015.
+ */
+public class NewClass {
+    public static int someMethod(int x, int y) {
+        return x * y;
+    }
+}
---
+++ src/test/java/bionic/TestLesson3/FirstTest.java
@@ -0,0 +1,14 @@
+package bionic.TestLesson3;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by c2612 on 11.02.2015.
+ */
+public class FirstTest {
+
+    @Test
+    public void testNewClass() {
+
+    }
+}
