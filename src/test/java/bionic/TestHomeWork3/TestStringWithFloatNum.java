4d8a08fb60257fd9fbbc0c79dccd714b73c008a4
Andrey-Burkivskiy <andrey.bukivskiy@gmail.com>
2015-02-18 18:10:32 +0200

    add @Parameters to lesson4

--- src/test/java/bionic/TestHomeWork3/TestStringWithFloatNum.java
+++ src/test/java/bionic/TestHomeWork3/TestStringWithFloatNum.java
@@ -5,7 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestStringWithFloatNum {
-    @Test
+    @Test(invocationCount = 5)
     public void testStringWithFloatNum() {
         String stringFromFile = ReadTextFile.readFile("file_homework3.txt");
         Assert.assertEquals(stringFromFile.matches(".*( |^)(\\(?)[0-9]+\\.[0-9]+(\\)?)( |$).*"), true, "Real number does not found in string: \"" + stringFromFile +"\".");
