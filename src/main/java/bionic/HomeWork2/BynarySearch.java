58ab1f6da13e679c6eeb3d821bae2d0e2f16cf62
Andrey-Burkivskiy <andrey.bukivskiy@gmail.com>
2015-02-19 14:24:11 +0200

    change test lesson4 and add two classes HomePage, URL

--- src/main/java/bionic/HomeWork2/BynarySearch.java
+++
@@ -1,9 +0,0 @@
-package bionic.HomeWork2;
-
-public class BynarySearch {
-    public static void main(String[] args) {
-        GenerateArray.initArray();
-        GenerateArray.fillArray();
-        SearchNumber.startSearch();
-    }
-}
---
+++ src/main/java/bionic/home_page/HomePage.java
@@ -0,0 +1,7 @@
+package bionic.home_page;
+
+/**
+ * Created by andrey on 19.02.15.
+ */
+public class HomePage {
+}
